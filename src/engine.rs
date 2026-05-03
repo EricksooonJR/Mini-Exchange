1d4da8c39d0d552653a37fdac2fc2f55946e1d00
EricksooonJR <102484621+EricksooonJR@users.noreply.github.com>
2026-05-03 13:47:35 -0600

    Create engine.rs

---
+++ src/engine.rs
@@ -0,0 +1,46 @@
+use crate::models::{Order, OrderType};
+
+#[derive(Default)]
+pub struct OrderBook {
+    pub buys: Vec<Order>,
+    pub sells: Vec<Order>,
+}
+
+impl OrderBook {
+    pub fn new() -> Self {
+        Self {
+            buys: vec![],
+            sells: vec![],
+        }
+    }
+
+    pub fn add_order(&mut self, order: Order) {
+        match order.order_type {
+            OrderType::Buy => self.buys.push(order),
+            OrderType::Sell => self.sells.push(order),
+        }
+    }
+
+    pub fn match_orders(&mut self) {
+        self.buys.sort_by(|a, b| b.price.partial_cmp(&a.price).unwrap());
+        self.sells.sort_by(|a, b| a.price.partial_cmp(&b.price).unwrap());
+
+        while !self.buys.is_empty() && !self.sells.is_empty() {
+            let buy = &self.buys[0];
+            let sell = &self.sells[0];
+
+            if buy.price >= sell.price {
+                println!(
+                    "Trade executed: {} units at price {}",
+                    buy.quantity.min(sell.quantity),
+                    sell.price
+                );
+
+                self.buys.remove(0);
+                self.sells.remove(0);
+            } else {
+                break;
+            }
+        }
+    }
+}
