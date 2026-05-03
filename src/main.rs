567e596ab644fc6ef1fb5f2e01c017e516e7c78f
EricksooonJR <102484621+EricksooonJR@users.noreply.github.com>
2026-05-03 13:51:38 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
 mod engine;
 mod models;
 mod routes;
+mod ws;
 
 use axum::Router;
 use std::sync::{Arc, Mutex};
 use tokio::net::TcpListener;
+use tokio::sync::broadcast;
 
 use engine::OrderBook;
+use ws::WsState;
 
 #[tokio::main]
 async fn main() {
     let orderbook = Arc::new(Mutex::new(OrderBook::new()));
 
-    let app = routes::create_routes(orderbook);
+    // Canal para WebSocket
+    let (tx, _) = broadcast::channel(100);
+
+    let ws_state = WsState { tx };
+
+    let app = routes::create_routes(orderbook, ws_state);
 
     let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
